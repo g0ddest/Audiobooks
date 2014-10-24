b6e0d5f045ace5ad78084eaa69861d7729c4353c
Vitaliy <vitaliy.velikodniy@goritchy.com>
2014-10-24 12:09:17 +0300

    #4 own adapter

--- app/src/main/java/org/fantasy_worlds/audiobooks/Main.java
+++ app/src/main/java/org/fantasy_worlds/audiobooks/Main.java
@@ -2,14 +2,19 @@ package org.fantasy_worlds.audiobooks;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import org.fantasy_worlds.audiobooks.dbo.Author;
 import org.fantasy_worlds.audiobooks.dbo.Media;
@@ -27,6 +32,44 @@ public class Main extends Activity {
 
     private static final String TITLE = "title";
     private static final String AUTHOR = "author";
+
+    private class MediaAdapter extends ArrayAdapter<Media> {
+
+        private List<Media> items;
+
+        public MediaAdapter(Context context, int textViewResourceId, List<Media> items) {
+            super(context, textViewResourceId, items);
+            this.items = items;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                //inflate a new view for your list item
+                LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.booklist_item, null);
+            }
+            Media media = items.get(position);
+            if (media != null) {
+                //set text to view
+                TextView title = (TextView) v.findViewById(R.id.title);
+                TextView author = (TextView) v.findViewById(R.id.author);
+                if (title != null) {
+                    title.setText(media.MediaTitle);
+                }
+                if(author != null){
+                    try {
+                        Author authorObj = HelperFactory.getHelper().getAuthorDao().getAuthorById(media.AuthorId);
+                        author.setText(authorObj.Name + " " + authorObj.Surname);
+                    }catch (SQLException e){
+                        author.setText("");
+                    }
+                }
+            }
+            return v;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,21 +94,7 @@ public class Main extends Activity {
 
         }
 
-        for(Media media : medias){
-            hm = new HashMap<String, Object>();
-            hm.put(TITLE, media.MediaTitle); // Название
-            try {
-                Author author = HelperFactory.getHelper().getAuthorDao().getAuthorById(media.AuthorId);
-                hm.put(AUTHOR, author.Name + " " + author.Surname); // Автор
-            }catch (SQLException e){
-                hm.put(AUTHOR, "");
-            }
-            booksList.add(hm);
-        }
-
-        SimpleAdapter adapter = new SimpleAdapter(this, booksList,
-                R.layout.booklist_item, new String[] { TITLE, AUTHOR },
-                new int[] { R.id.title, R.id.author });
+        MediaAdapter adapter = new MediaAdapter(this, R.layout.booklist_item, medias);
 
         booksView.setAdapter(adapter);
 
@@ -73,9 +102,9 @@ public class Main extends Activity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
-                HashMap item = (HashMap) adapterView.getItemAtPosition(i);
+                Media item = (Media) adapterView.getItemAtPosition(i);
                 builder.setTitle("Сообщение")
-                        .setMessage("Выбрана книга "  + item.get(AUTHOR) + " : " + item.get(TITLE))
+                        .setMessage("Выбрана аудиокнига ID:"  + item.Id + " TITLE: " + item.MediaTitle + "(" + item.BookTitle + ")")
                         .setCancelable(false)
                         .setNegativeButton("ОК",
                                 new DialogInterface.OnClickListener() {
