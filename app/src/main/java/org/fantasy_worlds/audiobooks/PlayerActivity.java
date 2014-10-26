fbeaf9b3c0792a19a75bec62a756ec472187d66b
Vitaliy <vitaliy@velikodniy.name>
2014-10-26 13:46:54 +0200

    little refactor

--- app/src/main/java/org/fantasy_worlds/audiobooks/PlayerActivity.java
+++ app/src/main/java/org/fantasy_worlds/audiobooks/PlayerActivity.java
@@ -1,10 +1,10 @@
 package org.fantasy_worlds.audiobooks;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,16 +14,17 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
 import com.androidquery.callback.AjaxStatus;
-import com.androidquery.callback.BitmapAjaxCallback;
 
-import org.fantasy_worlds.audiobooks.dbo.Author;
 import org.fantasy_worlds.audiobooks.dbo.Media;
 import org.fantasy_worlds.audiobooks.dbo.MediaPart;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,11 +44,18 @@ public class PlayerActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View v = convertView;
+            final ImageView loadStatus;
+            final ProgressBar mediaLoadBar;
             if (v == null) {
                 //inflate a new view for your list item
                 LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 v = vi.inflate(R.layout.medialist_item, null);
             }
+
+            loadStatus = (ImageView) v.findViewById(R.id.load_status);
+            mediaLoadBar = (ProgressBar) v.findViewById(R.id.media_progressbar);
+
+            AQuery aq = new AQuery(v);
 
             MediaPart mediaPart = items.get(position);
             if (mediaPart != null) {
@@ -57,6 +65,41 @@ public class PlayerActivity extends Activity {
                     title.setText(mediaPart.Title);
                 }
             }
+
+            assert mediaPart != null;
+            aq.progress(R.id.media_progressbar).ajax(mediaPart.Path, File.class, new AjaxCallback<File>(){
+
+                public void callback(String url, File file, AjaxStatus status) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(PlayerActivity.this);
+
+                    loadStatus.setImageDrawable(getResources().getDrawable(R.drawable.ic_ok));
+                    mediaLoadBar.setVisibility(View.GONE);
+
+                    String message;
+
+                    if(file != null){
+                        message = "File:" + file.length() + ":" +  status;
+                    }else{
+                        message = "Failed" + status;
+                    }
+
+                    builder.setTitle("Сообщение")
+                            .setMessage(message)
+                            .setCancelable(false)
+                            .setNegativeButton("ОК",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+
+                }
+
+            });
+
             return v;
         }
     }
@@ -96,9 +139,6 @@ public class PlayerActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 }
